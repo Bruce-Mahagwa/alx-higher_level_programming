a1edf4bd9eeacc69ab8040b8220987443a94753b
Bruce <jacobbruce880@gmail.com>
2022-08-23 04:42:26 -0700

    Fixes pycode style issues

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,9 @@ def print_last_digit(number):
     if (number < 0):
         num = number * -1
         num = num % 10
-        print("{}".format(num), end = "")
+        print("{}".format(num), end="")
         return num
     else:
         num = number % 10
-        print("{}".format(num), end = "")
+        print("{}".format(num), end="")
         return num
