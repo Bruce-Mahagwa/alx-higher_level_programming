e3b84ac8e860d52c9a8f030eede4b46b774f18f1
Bruce <jacobbruce880@gmail.com>
2022-08-25 04:38:08 -0700

    Fixes execution issues

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-for x in dir(hidden_4):
-    if x[:2] == "__":
-        continue
-    print(x)
+if __name__ == "__main__":
+    import hidden_4
+    for x in dir(hidden_4):
+        if x[:2] == "__":
+            continue
+        print(x)
