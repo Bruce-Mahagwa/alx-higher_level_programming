6b37d65834a6f299be28d2214f82d1ab955804b2
Bruce <jacobbruce880@gmail.com>
2022-08-25 04:36:01 -0700

    Solves execution issues

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-from sys import argv
-if len(argv) < 4:
-    print("Usage: ./100-my_calculator.py <a> operator <b>")
-    quit(1)
-arr = ["+", "-", "/", "*"]
-if argv[2] not in arr:
-    print("Unknown operator. Available operators: +, -, * and /")
-    quit(1)
-if argv[2] == "+":
-    print("{} + {} = {}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
-elif argv[2] == "-":
-    print("{} - {} = {}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
-elif argv[2] == "*":
-    print("{} * {} = {}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
-else:
-    print("{} / {} = {}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) < 4:
+        print("Usage: ./100-my_calculator.py <a> operator <b>")
+        quit(1)
+    arr = ["+", "-", "/", "*"]
+    if argv[2] not in arr:
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
+    if argv[2] == "+":
+        print("{} + {} = {}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "-":
+        print("{} - {} = {}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "*":
+        print("{} * {} = {}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
+    else:
+        print("{} / {} = {}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-num = 0
-for i in argv:
-    if i == argv[0]:
-        continue
-    num += int(i)
+if __name__ == "__main__":
+    from sys import argv
+    num = 0
+    for i in argv:
+        if i == argv[0]:
+            continue
+        num += int(i)
 print(num)
