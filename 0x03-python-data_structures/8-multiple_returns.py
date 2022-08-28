eafc9c7e1fd2a83c181cedff9b8c0c610f392b0d
Bruce <jacobbruce880@gmail.com>
2022-08-28 06:20:10 -0700

    Prints string length

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     strlen = len(sentence)
-    if sentence == "":
+    if strlen == 0:
         strlen = None
     char1 = sentence[0]
     myTuple = strlen, char1
