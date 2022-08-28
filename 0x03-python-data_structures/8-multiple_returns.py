4bc8e1a7a5f008b358482501af7e54164ca7cc9c
Bruce <jacobbruce880@gmail.com>
2022-08-28 05:29:48 -0700

    Checks whether a number is divisible by 2 or not

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     strlen = len(sentence)
+    if strlen == 0:
+        strlen = None
     char1 = sentence[0]
     myTuple = strlen, char1
     return myTuple
