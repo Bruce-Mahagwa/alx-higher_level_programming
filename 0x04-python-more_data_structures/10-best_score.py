2bd02400a0837c75e5500e00f43faa690c260ada
Bruce <jacobbruce880@gmail.com>
2022-08-31 05:25:39 -0700

    Fixes issues with styling

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary):
+    if a_dictionary and len(a_dictionary):
         max = a_dictionary[list(a_dictionary)[0]]
+        str = ""
         for k, v in a_dictionary.items():
             if a_dictionary[k] > max:
-                max = k
-        return max
+                str = k
+        return str
     return None
