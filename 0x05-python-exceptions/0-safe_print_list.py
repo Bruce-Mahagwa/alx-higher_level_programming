7a152c61dd353312bd689ed59278fcba5d6fbd8a
Bruce <jacobbruce880@gmail.com>
2022-09-12 01:34:31 -0700

    Prints elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#if __name__ == "__main__":
+def safe_print_list(my_list=[], x=0):
+    count = 0;
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
