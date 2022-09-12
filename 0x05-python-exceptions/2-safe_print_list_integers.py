c396f7a54d31d1ad335d84083ef6d8eab652d5c6
Bruce <jacobbruce880@gmail.com>
2022-09-12 03:04:54 -0700

    Divides 2 ints

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    arr = []
-    for j in my_list:
-        if type(j) == int:
-            arr.append(j)
     for i in range(x):
         try:
             print("{:d}".format(arr[i]), end = "")
             count += 1
-        except IndexError:
-            break
+        except (ValueError, TypeError):
+            continue
     print()
     return count
