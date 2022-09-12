39769d4566195f2bb11564fba1b930198cf31d9a
Bruce <jacobbruce880@gmail.com>
2022-09-12 04:48:06 -0700

    Fixes pycode style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-#if __name__ == "__main__":
 def safe_print_list(my_list=[], x=0):
-    count = 0;
+    count = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
