5db7122906b1816b3048b7282d29dc435ea0b691
Bruce <jacobbruce880@gmail.com>
2022-09-14 01:06:44 -0700

    Uses getters and setters to set value of length of a square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""A square class"""
+
+class Square:
+    """Represents a Square
+
+    Atributes:
+        __size(int): side of the square
+    """
+    def __init__(self, size = 0):
+        """initializes a square with side of size
+
+        Args:
+            size(int): side of the squre
+
+        Returns:
+            None
+        """
+        self.size = size
+    def area(self):
+        """calculates area
+
+        Returns: Area
+        """
+        return self.__size ** 2
+    @property
+    def size(self):
+        """gets value of size
+
+        Returns:
+            Value of size
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """setter function method
+
+        Args:
+            value(int): length of the square
+
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+
