bcd5e43e4ca4c589a6a4ba63a40084b5b58f374a
Bruce <jacobbruce880@gmail.com>
2022-09-19 03:22:29 -0700

    Creates a rect class

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
