1af9b2c9af226560488c92bc51c1d90dc2ea5b37
Bruce <jacobbruce880@gmail.com>
2022-11-28 00:14:50 -0800

    Prints the first arg orno argument if it doesn't exist

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints first argument if it exists ele print no argument
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
