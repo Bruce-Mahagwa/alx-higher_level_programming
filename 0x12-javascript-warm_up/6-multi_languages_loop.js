d371b84d753acac59f94ef0179b64370e5376bb4
Bruce <jacobbruce880@gmail.com>
2022-11-28 00:50:52 -0800

    Prints strings using loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints 3 string susing a loop
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
