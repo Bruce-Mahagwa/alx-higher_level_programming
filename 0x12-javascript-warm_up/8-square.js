9f458d07ffe592836e47f0d604ae03b36e0709e8
Bruce <jacobbruce880@gmail.com>
2022-11-28 01:15:56 -0800

    Prints a square based on argument value

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints a square
+const args = process.argv[2];
+if (isNaN(args)) {
+  console.log('Missing size');
+} else {
+  let str = 'x'.repeat(parseInt(args));
+  for (let i = 0; i < parseInt(args); i++) {
+    console.log(str);
+  }
+}
