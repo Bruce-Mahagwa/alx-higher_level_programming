5117f2c39b1ef2857ae9408527804afa00fa30e1
Bruce <jacobbruce880@gmail.com>
2022-11-29 04:49:06 -0800

    Js styling corrections

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const args = process.argv[2];
 if (isNaN(args)) {
   console.log('Missing size');
 } else {
-  let str = 'x'.repeat(parseInt(args));
+  let str = 'X'.repeat(parseInt(args));
   for (let i = 0; i < parseInt(args); i++) {
     console.log(str);
   }
